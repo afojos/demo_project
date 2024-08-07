fc88589f22cb9de3e6701155d53e4655d8cfe3d5
afojos <afolayanjoshua2000@gmail.com>
2024-08-07 19:56:46 +0100

    Deploy site

--- www/index.php
+++ www/index.php
@@ -1,5 +1,13 @@
 <?php
-echo "Welcome adesoye";
+//echo $_SERVER['REQUEST_URI'];
+
+define("APP_PATH",dirname(dirname(__FILE__)));
+
+//add database connection
+
+include APP_PATH."/model/model.php";
+include APP_PATH."/controller/controller.php";
+include APP_PATH."/route/router.php";
 
 
 ?>
